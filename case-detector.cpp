20ee9fdd999d8846d179d2f1bd7787334427e4c0
TnAgMutNjaTurtl <joseph.d.pope@gmail.com>
2020-02-27 00:05:36 -0600

    fixed case-detector command line and isdigit

--- case-detector.cpp
+++ case-detector.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 
-int main()
+int main(int argc, char*argv[])
 {
+    if (argc <= 1)
+    {
+        cout << "Please only input letters for this program." << endl;
+        return 0;
+    }
     int value = 0;
-    char letter;
-    cout << "Please Enter a Letter upper case or lower case" << endl;
-    cin >> letter;
+    char letter = argv[1][0];
 
-    if (islower(letter))
+    if (strlen(argv[1]) > 1)
+        {
+            letter = ':';
+
+            cout << "Please only input letters for this program." << endl;
+            return 0;
+        }
+    if(isdigit(letter))
+    {
+        cout << "Please only input letters for this program." << endl;
+        return 0;
+    }
+    
+    if (islower(*argv[1]))
     {
         cout << letter << " is lower case" << endl;
         value = 1;
